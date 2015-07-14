b275778a811dcc5968e8aba13846956102b1851d
fiiv <mike.timofiiv@gmail.com>
2015-07-14 17:59:30 +0200

    fixing up a test and some ref

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@
 var expect = require('chai').expect;
 
 var mongoose = require('mongoose');
+require('mockgoose')(mongoose);
+mongoose.connect('mongodb://localhost/test');
 
 var IbanModule = require('../index.js');
 var IbanType = IbanModule.loadType(mongoose);
@@ -20,28 +22,61 @@ describe('Module loads in a new type', function() {
 });
 
 describe('On validation', function() {
+  before(function(done) {
+    mongoose.connect('mongodb://localhost/test');
+    mongoose.connection.on('error', function(){});
+    mongoose.connection.once('open', done);
+  });
+
+  after(function() {
+    mongoose.connection.close();
+  });
+
   var Iban = Schema.Types.Iban;
   var SampleSchema = new Schema({ iban: { type: Iban } });
   var SampleModel = mongoose.model('Sample', SampleSchema);
 
   it('throws an error when given a non-string', function() {
     var Model = new SampleModel({ iban: 22 });
-    expect(Model.validate).to.throw(Error);
+
+    Model.save(function(err) {
+      expect(err.errors.iban.message).to.equal('IBAN is invalid');
+    });
   });
 
   it('throws an error when given an invalid IBAN', function() {
     var Model = new SampleModel({ iban: 'NLfff' });
-    expect(Model.validate).to.throw(Error);
+
+    Model.save(function(err) {
+      expect(err.errors.iban.message).to.equal('IBAN is invalid');
+    });
   });
 
   it('does not complain when a proper IBAN is given', function() {
     var Model = new SampleModel({ iban: 'NL39 RABO 0300 0652 64' });
-    expect(Model.validate).to.throw(Error);
+
+    Model.save(function(err) {
+      expect(err).to.be.null;
+    });
   });
 
-  it('stores the proper IBAN value', function() {
+  it('stores the proper IBAN value without any validation errors', function() {
     var Model = new SampleModel({ iban: 'NL39 RABO 0300 0652 64' });
-    Model.validate();
-    expect(Model.iban).to.equal('NL39 RABO 0300 0652 64');
+
+    Model.save(function(err) {
+      expect(err).to.be.null;
+    });
   });
+});
+
+describe('On save', function() {
+  before(function(done) {
+		mongoose.connect('mongodb://localhost/test');
+		mongoose.connection.on('error', function(){});
+		mongoose.connection.once('open', done);
+	});
+
+	after(function() {
+		mongoose.connection.close();
+	});
 });
